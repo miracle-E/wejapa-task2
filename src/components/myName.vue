3b3ce1a701da50024cb285532dc34fff49edac5b
miracle-E <miraclekalu1212@gmail.com>
2020-07-29 10:32:52 +0100

    Corrected mistakes

--- src/components/myName.vue
+++ src/components/myName.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="hello">
+  <div>
     <h1>{{ name }}</h1>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'myName',
   props: {
     name: String
   }
